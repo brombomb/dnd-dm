90343dafe59197d75e155b0a474c02dcd6065e26
Rob Walsh <brombomb@gmail.com>
2025-03-24 23:11:14 -0600

    dark mode

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,12 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { CssBaseline } from '@mui/material';
 
-const theme = createTheme();
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
       <App />
     </ThemeProvider>
   </React.StrictMode>
